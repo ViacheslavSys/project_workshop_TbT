61aae4309743b9554ac2e6646e1bfbc97707e22d
JellyBean-bit <99984027+ViacheslavSys@users.noreply.github.com>
2025-10-11 19:12:29 +0500

    change: test_main.py, yml

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,7 @@ from app.api.routes_user import router as user_router
 
 app = FastAPI(title="My Project", version="0.1.0")
 
-app.include_router(user_router, prefix="/api")
-app.include_router(health_router, prefix="/api")
-app.include_router(assets_router, prefix="/api")
-app.include_router(portfolios_router, prefix="/api")
+app.include_router(user_router)
+app.include_router(health_router)
+app.include_router(assets_router)
+app.include_router(portfolios_router)
